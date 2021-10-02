f7bc325287362a2d4dc9577a8cf3dd9c54d72d93
sweethoneybee <jsjphone8@gmail.com>
2021-10-02 23:40:30 +0900

    Creating a macOS App - 맥앱 만들어보기
    1. #if os(macOS, watchOS) 등의 전처리문으로 분기 나누기
    2. 전부터 나오던 거지만 열거형이 String, CaseIterable, Identifiable을 채택하게 해서 ForEach 문에 사용하는 테크닉 자주 사용되는 듯
    3. ToolbarItem > Menu 에서 label이 특이한 문법으로 붙음. LandmarkList.swift 참고할 것

---
+++ Landmarks/MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by 정성훈 on 2021/10/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
